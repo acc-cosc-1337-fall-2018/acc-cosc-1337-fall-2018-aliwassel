3e5420c469ed3373aeaa86b468c5b80d38092b7d
aliwassel <42983872+aliwassel@users.noreply.github.com>
2018-09-13 19:48:28 -0500

    Update basics.cpp

--- 01-basics/basics.cpp
+++ 01-basics/basics.cpp
@@ -8,7 +8,7 @@ int sum_numbers(int num1, int num2)
 //write code for multiply_numbers function here
 int multiply_numbers(int num1, int num2)
 {
-	return num1*num2
+	return num1*num2;
 }
 
 void pass_by_val_by_ref(int val, int& ref)
