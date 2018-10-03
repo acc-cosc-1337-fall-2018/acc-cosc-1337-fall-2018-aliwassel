f01c556813f97e5ed4eba2d8d2f6074ae86fceca
ALi Wassel <ali_wasil@hotmail.com>
2018-10-02 20:59:00 -0500

    Changes repository.

---
+++ homeworks/05-classes/tic_tac_toe.h
@@ -0,0 +1,32 @@
+#ifndef TIC_TAC_TOE_H
+#define TIC_TAC_TOE_H
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+class TicTacToeBoard
+{
+public:
+
+	bool game_over;
+	void start_game(string player);
+	void mark_board(int position);
+	string get_player();
+	void display_board();
+
+private:
+	void set_next_player();
+	bool check_column_win();
+	bool check_row_win();
+	bool check_diagonal_win();
+	void clear_board();
+	void check_board)_full();
+	vector<vector <string>>pegs;
+
+	string next_player;
+
+
+	vector<vector<int>>pegs(9, "");
+};
+
+#endif // TIC_TAC_TOE_H
---
+++ homeworks/05-classes/tic_tac_toe_board.cpp
@@ -0,0 +1,65 @@
+//#include"tic_tac_toe_board.h"
+//
+//void TicTacToeBoard::start_game(string player) 
+//{
+//	for (auto s : player) {
+//		if player == 'X' || player == 'O'
+//		{
+//			next_player = player;
+//		}
+//			
+//		
+//		clear_board();
+//	}
+//}
+//
+//void TicTacToeBoard::mark_board(int position) 
+//{
+//	pegs[postion] = next_player;
+//	set_next_player();
+//
+//}
+//string TicTacToeBoard::get_player()
+//{
+//	return next_player;
+//}
+//void TicTacToeBoard::display_board() 
+//{
+//	cout << "You marked : " << next_player << "at: " << pegs[position] << endl;
+//
+//}
+//
+//void TicTacBoard::set_next_player() 
+//{
+//	if (next_player == 'X') 
+//	{
+//		next_player = 'O';
+//
+//	}
+//	else if (next_player == 'O') 
+//	{
+//		next_player = 'X';
+//	}
+//}
+//
+////bool TicTacToeBoard::check_column_win() 
+////{
+////	for (auto n : pegs) 
+////	{
+////
+////	}// skip for now
+////
+////}
+//
+//
+//bool TicTacoToeBoard::check_row_win() 
+//{
+//	bool flag;
+//	flag = false;
+//
+//	//A row wins with marked values 1, 2, 3 or 4, 5, 6 or 7, 8, 9 with all Os or Xs
+//	if (peges[1] == 'X' && pegs[3] == 'X' && pegs[] == 'X')
+//
+//	else if (pegs[0] == 'O' && peges[1]=='O' && pegs[2]=='O')
+//
+//}
